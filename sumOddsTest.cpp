f6691f7af8867befb24c2962a2660e3dc8716620
Wong Zhao <193191274+wongzhao@users.noreply.github.com>
2025-05-02 15:49:53 -0700

    Update test instructions and remove +x

--- sumOddsTest.cpp
+++ sumOddsTest.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-  // Fill this in with exactly four tests that determine whether your
+  // Fill this in with exactly seven tests that determine whether your
   // code for sumOdds() correctly sums up all the odd numbers in
   // arrays of various sizes.  Include both positive, negative and
   // zero values in your test arrays.
