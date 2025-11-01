ef89c0fc2c177da307a4da23c9482eecfd147335
Brielle <bmcbarron@ucsb.edu>
2025-10-31 20:04:01 -0700

    implemented utility functions -- isOdd() , isEven() , isPrime()

--- utility.cpp
+++ utility.cpp
@@ -3,19 +3,40 @@
 // IN THIS FILE, define any of your OWN functions you may need to 
 // solve the problems.    
 
-// For example, if you need an isPrime function, you can put the function
-// definition in this file.  Similarly, isOdd or isEven might be useful.
-
 // You will need to include the function prototype in "utility.h" and
 // then be sure to  #include "utility.h" in the file where you use
 // these functions
 
-bool isOdd(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+#include <iostream>
+using namespace std;
+
+
+bool isOdd(int x) {
+  if (x % 2 != 0) {
+    return true;
+  }	  
+  return false;
 }
 bool isEven(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+  if (x%2 == 0) {
+	  return true;
+  }
+  return false;
 }
-bool isPrime(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+bool isPrime(int x) {
+  if (x < 0) {
+    x = -x; //if it's negative, make it positive; won't matter either way
+  }
+
+  if (x == 1 || x == 0) {
+    return false;
+  }
+
+
+  for (int index = 2; index < x; index ++) {
+    if (x % index == 0) {
+	    return false;
+    }  
+  }
+  return true;
 }
